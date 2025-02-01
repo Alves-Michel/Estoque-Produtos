7219f15f0b231fd061f010ae62f2a0de56f455cd
MICHEL OLIVEIRA <121099676+Alves-Michel@users.noreply.github.com>
2025-02-01 11:19:01 -0300

    Update Conection.java

--- src/main/java/pkg8/app/estoque/conection/Conection.java
+++ src/main/java/pkg8/app/estoque/conection/Conection.java
@@ -1,1 +1,27 @@
-package pkg8.app.estoque.conection;import java.sql.Connection;import java.sql.DriverManager;import java.sql.SQLException;public class Conection {    private static final String url = "jdbc:mysql://localhost:3306/dadosprodutos";    private static final String user = "root";    private static final String password = "root";    private static Connection conn;    public Conection() {    }    public static Connection getConexao() {        try {            if (conn == null) {                conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/dadosprodutos", "root", "root");                return conn;            } else {                return conn;            }        } catch (SQLException var1) {            var1.printStackTrace();            return null;        }    }}
+package pkg8.app.estoque.conection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conection {
+    private static final String URL = "jdbc:mysql://localhost:3306/dadosprodutos";
+    private static final String USER = "root";
+    private static final String PASSWORD = "root";
+    private static Connection conn;
+
+    private Conection() {
+        // Construtor privado para evitar instanciação
+    }
+
+    public static Connection getConexao() {
+        try {
+            if (conn == null || conn.isClosed()) {
+                conn = DriverManager.getConnection(URL, USER, PASSWORD);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return conn;
+    }
+}
