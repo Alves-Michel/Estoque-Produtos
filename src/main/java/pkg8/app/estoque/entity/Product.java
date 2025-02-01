04613869e8530002da3f90c15b287c4b60e4ccd3
MICHEL OLIVEIRA <121099676+Alves-Michel@users.noreply.github.com>
2025-02-01 11:17:49 -0300

    Update Product.java

--- src/main/java/pkg8/app/estoque/entity/Product.java
+++ src/main/java/pkg8/app/estoque/entity/Product.java
@@ -1,1 +1,143 @@
-package pkg8.app.estoque.entity;import java.text.NumberFormat;import java.util.Locale;public class Product {    Locale localeBR = new Locale("pt", "BR");    NumberFormat currencyFormat;    public String NameProduct;    public double Price;    public int Amount;    public int Codigo;    public Product() {        this.currencyFormat = NumberFormat.getCurrencyInstance(this.localeBR);    }    public Product(String nameProduct, double price, int amount, int codigo) {        this.currencyFormat = NumberFormat.getCurrencyInstance(this.localeBR);        this.NameProduct = nameProduct;        this.Price = price;        this.Amount = amount;        this.Codigo = codigo;    }    public String getNameProduct() {        return this.NameProduct;    }    public double getPrice() {        return this.Price;    }    public int getAmount() {        return this.Amount;    }    public int getCodigo() {        return this.Codigo;    }    public void setNameProduct(String nameProduct) {        this.NameProduct = nameProduct;    }    public void setPrice(double price) {        this.Price = price;    }    public void setAmount(int amount) {        this.Amount = amount;    }    public void setCodigo(int codigo) {        this.Codigo = codigo;    }    public double ValorEmEstoque() {        return this.Price * (double)this.Amount;    }    public double CalcularTotalEmEstoque() {        return this.Price * (double)this.Amount;    }    public String toString() {        return "        C??digo-" + this.Codigo + "- Produto: " + this.NameProduct + ", " + this.currencyFormat.format(this.Price) + ", Quantidade: " + this.Amount;    }}
+package pkg8.app.estoque.entity;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+
+public class Product {
+    private Locale localeBR = new Locale("pt", "BR");
+    private NumberFormat currencyFormat;
+    private String nameProduct;
+    private double price;
+    private int amount;
+    private int codigo;
+
+    public Product() {
+        this.currencyFormat = NumberFormat.getCurrencyInstance(this.localeBR);
+    }
+
+    public Product(String nameProduct, double price, int amount, int codigo) {
+        this.currencyFormat = NumberFormat.getCurrencyInstance(this.localeBR);
+        this.nameProduct = nameProduct;
+        this.price = price;
+        this.amount = amount;
+        this.codigo = codigo;
+    }
+
+    public String getNameProduct() {
+        return this.nameProduct;
+    }
+
+    public double getPrice() {
+        return this.price;
+    }
+
+    public int getAmount() {
+        return this.amount;
+    }
+
+    public int getCodigo() {
+        return this.codigo;
+    }
+
+    public void setNameProduct(String nameProduct) {
+        this.nameProduct = nameProduct;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public double valorEmEstoque() {
+        return this.price * this.amount;
+    }
+
+    public double calcularTotalEmEstoque() {
+        return this.price * this.amount;
+    }
+
+    @Override
+    public String toString() {
+        return "Código-" + this.codigo + " - Produto: " + this.nameProduct + ", " + this.currencyFormat.format(this.price) + ", Quantidade: " + this.amount;
+    }
+}
+package pkg8.app.estoque.entity;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+
+public class Product {
+    private Locale localeBR = new Locale("pt", "BR");
+    private NumberFormat currencyFormat;
+    private String nameProduct;
+    private double price;
+    private int amount;
+    private int codigo;
+
+    public Product() {
+        this.currencyFormat = NumberFormat.getCurrencyInstance(this.localeBR);
+    }
+
+    public Product(String nameProduct, double price, int amount, int codigo) {
+        this.currencyFormat = NumberFormat.getCurrencyInstance(this.localeBR);
+        this.nameProduct = nameProduct;
+        this.price = price;
+        this.amount = amount;
+        this.codigo = codigo;
+    }
+
+    public String getNameProduct() {
+        return this.nameProduct;
+    }
+
+    public double getPrice() {
+        return this.price;
+    }
+
+    public int getAmount() {
+        return this.amount;
+    }
+
+    public int getCodigo() {
+        return this.codigo;
+    }
+
+    public void setNameProduct(String nameProduct) {
+        this.nameProduct = nameProduct;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public double valorEmEstoque() {
+        return this.price * this.amount;
+    }
+
+    public double calcularTotalEmEstoque() {
+        return this.price * this.amount;
+    }
+
+    @Override
+    public String toString() {
+        return "Código-" + this.codigo
+          + " - Produto: " + this.nameProduct +
+          ", " + this.currencyFormat.format(this.price) +
+          ", Quantidade: " + this.amount;
+    }
+}
